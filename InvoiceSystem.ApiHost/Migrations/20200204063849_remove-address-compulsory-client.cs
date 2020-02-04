a07e467d696d73c0692f8ddab7e7e68c79c3a330
joachim.jissang <joachim.jissang@live.com>
2020-02-03 20:50:29 -1000

    remove the compulsory address on customer field

---
+++ InvoiceSystem.ApiHost/Migrations/20200204063849_remove-address-compulsory-client.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceSystem.ApiHost.Migrations
+{
+    public partial class removeaddresscompulsoryclient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdressePhysique",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdressePhysique",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
